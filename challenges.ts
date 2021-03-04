20a6c001c9e27e7424369a3693868910d3911eab
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-04 01:50:05 -0500

    solution for sakura pedal function

---
+++ challenges.ts
@@ -0,0 +1,19 @@
+/* 
+Challenge One: When it's spring Japanese cherries blossom, it's called "sakura" and it's admired a lot. The petals start to fall in late April. Suppose that the falling speed of a petal is 5 centimeters per second (5 cm/s), and it takes 80 seconds for the petal to reach the ground from a certain branch. Write a function that receives the speed (in cm/s) of a petal as input, and returns the time it takes for that petal to reach the ground from the same branch.
+Challenge Source: https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b/solutions/typescript
+*/
+
+// export function sakuraFall(v: number): number {
+//     let distanceFell = 400;
+//     if (v > 0) {
+//         let howManySeconds = distanceFell / v;
+//         return howManySeconds;
+//     } else {
+//         return 0;
+//     }
+// }
+
+// // TESTS
+// console.log(sakuraFall(5));
+// console.log(sakuraFall(8));
+// console.log(sakuraFall(-7));
