a72c3e87835e07caabb0dc3c6da6a3c250ace7ee
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-09-19 23:59:09 -0400

    added getCount solution

--- challenges.js
+++ challenges.js
@@ -4,7 +4,7 @@ Challenge One: When it's spring Japanese cherries blossom, it's called "sakura" 
 Challenge Source: https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b/solutions/typescript
 */
 exports.__esModule = true;
-exports.isValidWalk = void 0;
+exports.getCount = void 0;
 // export function sakuraFall(v: number): number {
 //     let distanceFell = 400;
 //     if (v > 0) {
@@ -62,33 +62,36 @@ Challenge Source: https://www.codewars.com/kata/57e3f79c9cb119374600046b/typescr
 // console.log(hello());
 // console.log(hello(""));
 /*
-Challenge Twenty-Seven: You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+Challenge Five: You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
 Challenge Source: https://www.codewars.com/kata/54da539698b8a2ad76000228/typescript
 */
-// function isValidWalk(walk) {
-//     var ns = 0;
-//     var we = 0;
-//     for (var _i = 0, walk_1 = walk; _i < walk_1.length; _i++) {
-//         var dir = walk_1[_i];
-//         if (dir == 'n')
-//             ns += 1;
-//         if (dir == 's')
-//             ns -= 1;
-//         if (dir == 'w')
-//             we += 1;
-//         if (dir == 'e')
-//             we -= 1;
-//     }
-//     if (ns === 0 && we === 0 && walk.length === 10) {
-//         return true;
-//     }
-//     else {
-//         return false;
-//     }
+// export function isValidWalk(walk: string[]) {
+//   let ns = 0;
+//   let we = 0;
+//   for (let dir of walk) {
+//     if (dir == 'n') ns += 1;
+//     if (dir == 's') ns -= 1;
+//     if (dir == 'w') we += 1;
+//     if (dir == 'e') we -= 1;
+//   }
+//   if (ns === 0 && we === 0 && walk.length === 10) {
+//     return true;
+//   } else {
+//     return false;
+//   }
 // }
-// exports.isValidWalk = isValidWalk;
 // // TESTS
-// console.log(isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']));
-// console.log(isValidWalk(['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e']));
+// console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']));
+// console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']));
 // console.log(isValidWalk(['w']));
-// console.log(isValidWalk(['n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's']));
+// console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']));
+// Challenge Six: Return the number (count) of vowels in the given string. We will consider a, e, i, o, u as vowels for this Kata (but not y). The input string will only consist of lower case letters and/or spaces.
+// Challenge Source: https://www.codewars.com/kata/54ff3102c1bad923760001f3/typescript
+function getCount(str) {
+    return str.replace(/[^aeiou]/gi, '').length;
+}
+exports.getCount = getCount;
+// TESTS
+console.log(getCount("vowel"));
+console.log(getCount('cover'));
+console.log(getCount('cranium'));
