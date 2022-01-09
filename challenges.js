292cd659cba5bb7e07edd9483f479026a6ca081e
jamesnascimento1994 <james.nascimento94@yahoo.com>
2022-01-08 22:37:35 -0500

    added solution for decode morse

--- challenges.js
+++ challenges.js
@@ -4,7 +4,7 @@ Challenge One: When it's spring Japanese cherries blossom, it's called "sakura" 
 Challenge Source: https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b/solutions/typescript
 */
 exports.__esModule = true;
-exports.descendingOrder = void 0;
+exports.decodeMorse = void 0;
 // export function sakuraFall(v: number): number {
 //     let distanceFell = 400;
 //     if (v > 0) {
@@ -103,15 +103,38 @@ Challenge Source: https://www.codewars.com/kata/571d42206414b103dc0006a1
 // console.log(arr());
 // console.log(arr(4));
 // console.log(arr(0));
-/* Challenge Seven: Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+/* Challenge Eight: Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 Challenge Source: https://www.codewars.com/kata/5467e4d82edf8bbf40000155
 */
-function descendingOrder(n) {
-    // your code here
-    return (parseFloat(n.toString().split('').sort().reverse().join('')) * Math.sign(n));
-}
-exports.descendingOrder = descendingOrder;
-// TESTS
-console.log(descendingOrder(42145));
-console.log(descendingOrder(145263));
-console.log(descendingOrder(123456789));
+// export function descendingOrder(n: number): number {
+//     // your code here
+//     return (
+//       parseFloat(
+//         n.toString().split('').sort().reverse().join('')
+//       ) * Math.sign(n)
+//     )
+// }
+// // TESTS
+// console.log(descendingOrder(42145));
+// console.log(descendingOrder(145263));
+// console.log(descendingOrder(123456789));
+/* Challenge Nine: In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
+The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+
+NOTE: Extra spaces before or after the code have no meaning and should be ignored.
+
+In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words. Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+Challenge Source: https://www.codewars.com/kata/54b724efac3d5402db00065e
+
+NOTE: The MORSE_CODE variable is exclusive to codewars. I'm not sure how to fix the error on VS code without having to create a giant array of every letter with its morse code.
+*/
+// function decodeMorse(morseCode) {
+//     // your code here
+//     var trimmedMorseCode = morseCode.trim();
+//     var splitMorseCode = trimmedMorseCode.split(/  | /);
+//     var mappedMorseCode = splitMorseCode.map(function (morse) { return MORSE_CODE[morse] || ' '; });
+//     var joinedMorseCode = mappedMorseCode.join('');
+//     return joinedMorseCode;
+// }
+// exports.decodeMorse = decodeMorse;
+// console.log(decodeMorse('.... . -.--   .--- ..- -.. .'));
--- challenges.ts
+++ challenges.ts
@@ -126,16 +126,39 @@ Challenge Source: https://www.codewars.com/kata/571d42206414b103dc0006a1
 Challenge Source: https://www.codewars.com/kata/5467e4d82edf8bbf40000155
 */
 
-export function descendingOrder(n: number): number {
-    // your code here
-    return (
-      parseFloat(
-        n.toString().split('').sort().reverse().join('')
-      ) * Math.sign(n)
-    )
-}
+// export function descendingOrder(n: number): number {
+//     // your code here
+//     return (
+//       parseFloat(
+//         n.toString().split('').sort().reverse().join('')
+//       ) * Math.sign(n)
+//     )
+// }
 
-// TESTS
-console.log(descendingOrder(42145));
-console.log(descendingOrder(145263));
-console.log(descendingOrder(123456789));
+// // TESTS
+// console.log(descendingOrder(42145));
+// console.log(descendingOrder(145263));
+// console.log(descendingOrder(123456789));
+
+/* Challenge Nine: In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
+The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+
+NOTE: Extra spaces before or after the code have no meaning and should be ignored.
+
+In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words. Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+Challenge Source: https://www.codewars.com/kata/54b724efac3d5402db00065e
+
+NOTE: The MORSE_CODE variable is exclusive to codewars. I'm not sure how to fix the error on VS code without having to create a giant array of every letter with its morse code.
+*/
+
+// export function decodeMorse(morseCode: string): string {
+//     // your code here
+//     let trimmedMorseCode = morseCode.trim();
+//     let splitMorseCode = trimmedMorseCode.split(/  | /);
+//     let mappedMorseCode = splitMorseCode.map((morse) =>  MORSE_CODE[morse] || ' ');
+//     let joinedMorseCode = mappedMorseCode.join('');
+    
+//     return joinedMorseCode;
+// }
+
+// console.log(decodeMorse('.... . -.--   .--- ..- -.. .'));
