69a917821fc0a2fab96023a544ae89a0a0e3a07c
jamesnascimento1994 <james.nascimento94@yahoo.com>
2023-05-02 17:42:28 -0400

    added solution for doors

--- challenges.js
+++ challenges.js
@@ -4,7 +4,7 @@ Challenge One: When it's spring Japanese cherries blossom, it's called "sakura" 
 Challenge Source: https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b/solutions/typescript
 */
 exports.__esModule = true;
-exports.Challenge = exports.pigIt = exports.duplicateCount = exports.descendingOrder = exports.arr = exports.getCount = exports.isValidWalk = exports.hello = exports.multiply = exports.disemvowel = exports.sakuraFall = void 0;
+exports.doors = exports.Challenge = exports.pigIt = exports.duplicateCount = exports.descendingOrder = exports.arr = exports.getCount = exports.isValidWalk = exports.hello = exports.multiply = exports.disemvowel = exports.sakuraFall = void 0;
 function sakuraFall(v) {
     var distanceFell = 400;
     if (v > 0) {
@@ -222,6 +222,20 @@ var Challenge = /** @class */ (function () {
 }());
 exports.Challenge = Challenge;
 // TESTS
-console.log(Challenge.solution(10));
-console.log(Challenge.solution(35));
-console.log(Challenge.solution(65));
+// console.log(Challenge.solution(10));
+// console.log(Challenge.solution(35));
+// console.log(Challenge.solution(65));
+/*
+Challenge Thirteen: In the morning all the doors in the school are closed. The school is quite big: there are N doors. Then pupils start coming. It might be hard to believe, but all of them want to study! Also, there are exactly N children studying in this school, and they come one by one. When these strange children pass by some doors they change their status (i.e. Open -> Closed, Closed -> Open). Each student has their number, and each i-th student alters the status of every i-th door. For example: when the first child comes to the schools, he changes every first door (he opens all of them). The second one changes the status of every second door (he closes some doors: the 2nd, the 4th and so on). Finally, when the last one – the n-th – comes to the school, he changes the status of each n-th door (there's only one such door, though).You need to count how many doors are left opened after all the students have come.
+Challenge Source: https://www.codewars.com/kata/57c15d314677bb2bd4000017
+*/
+var doors = function (n) {
+    return ~~Math.sqrt(n);
+};
+exports.doors = doors;
+// TESTS
+console.log(exports.doors(0));
+console.log(exports.doors(2));
+console.log(exports.doors(3));
+console.log(exports.doors(5));
+console.log(exports.doors(10));
